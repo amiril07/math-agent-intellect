a5b6ca7394747c3db3ec888b0bd0771e60b6094d
amiril07 <revinchism07@gmail.com>
2026-06-06 12:30:17 +0700

    UI Changes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     print("🚀 Menyalakan Server Math Agent Intellect...")
     print("🌍 Akses aplikasi di browser Anda melalui alamat: http://127.0.0.1:8000")
     
-    # Menjalankan aplikasi FastAPI 'app' yang ada di dalam src/web/app.py
     uvicorn.run(
         "src.web.app:app", 
         host="127.0.0.1", 
